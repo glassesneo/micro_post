4b7f00c36db93b32ca5fd499cd99274d8c4777f8
neo <glassesneo@protonmail.com>
2025-09-04 16:35:51 +0900

    feat(backend): implement post feature

--- backend/src/post/post.controller.ts
+++ backend/src/post/post.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from "@nestjs/common";
+import { PostService } from "./post.service";
 
-@Controller('post')
-export class PostController {}
+@Controller("post")
+export class PostController {
+	constructor(private readonly postService: PostService) {}
+	@Post()
+	async createPost(
+		@Body("message") message: string,
+		@Query("token") token: string,
+	) {
+		return await this.postService.createPost(message, token);
+	}
+
+	@Get()
+	async getList(
+		@Query("token") token: string,
+		@Query("start") start: number = 0,
+		@Query("records") records: number,
+	) {
+		return await this.postService.getList(token, start, records);
+	}
+}
--- backend/src/post/post.module.ts
+++ backend/src/post/post.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
-import { PostController } from './post.controller';
-import { PostService } from './post.service';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Auth } from "src/entities/auth";
+import { MicroPost } from "src/entities/microposts";
+import { PostController } from "./post.controller";
+import { PostService } from "./post.service";
 
 @Module({
-  controllers: [PostController],
-  providers: [PostService]
+	imports: [TypeOrmModule.forFeature([MicroPost, Auth])],
+	controllers: [PostController],
+	providers: [PostService],
 })
 export class PostModule {}
