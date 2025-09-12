dcdc187ba560963950874750c58eddf9dc0d5f4d
neo <glassesneo@protonmail.com>
2025-09-12 16:49:35 +0900

    refactor: introduce `@nestjs/config`

--- backend/src/app.datasource.ts
+++ backend/src/app.datasource.ts
@@ -1,13 +1,17 @@
+import { ConfigService } from "@nestjs/config";
 import { DataSource } from "typeorm";
+import { EnvironmentVariables } from "./app.module";
 
 require("dotenv").config();
 
+const configService = new ConfigService<EnvironmentVariables, true>();
+
 const AppDataSource = new DataSource({
 	type: "postgres",
-	host: process.env.DB_HOST,
-	username: process.env.DB_USER,
-	password: process.env.DB_PASS,
-	database: process.env.DB_NAME,
+	host: configService.get("DB_HOST"),
+	username: configService.get("DB_USER"),
+	password: configService.get("DB_PASS"),
+	database: configService.get("DB_NAME"),
 	entities: ["src/entities/*.ts"],
 	migrations: ["src/migrations/*.ts"],
 });
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
@@ -6,17 +7,31 @@ import { AuthModule } from "./auth/auth.module";
 import { PostModule } from "./post/post.module";
 import { UserModule } from "./user/user.module";
 
+export interface EnvironmentVariables {
+	DATABASE_URL: string;
+	DB_HOST: string;
+	DB_USER: string;
+	DB_PASS: string;
+	DB_NAME: string;
+}
+
 @Module({
 	imports: [
-		TypeOrmModule.forRoot({
-			type: "postgres",
-			url: process.env.DATABASE_URL,
-			host: process.env.DB_HOST,
-			username: process.env.DB_USER,
-			password: process.env.DB_PASS,
-			database: process.env.DB_NAME,
-			autoLoadEntities: true,
-			synchronize: false,
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (config: ConfigService<EnvironmentVariables, true>) => {
+				return {
+					type: "postgres",
+					host: config.get("DB_HOST"),
+					username: config.get("DB_USER"),
+					password: config.get("DB_PASS"),
+					database: config.get("DB_NAME"),
+					autoLoadEntities: true,
+					synchronize: false,
+					url: config.get("DATABASE_URL"),
+				};
+			},
 		}),
 		UserModule,
 		PostModule,
